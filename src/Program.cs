6b101977a143c967efe535ee8c1d135957ad45be
Mike Harder <mharder@microsoft.com>
2017-08-07 17:41:42 -0700

    Create application for 1.1.2 and 2.0.0

---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.PlatformAbstractions;
+using Newtonsoft.Json.Linq;
+
+namespace Plaintext
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(".NET Core " + GetNetCoreVersion());
+        }
+
+        public static string GetNetCoreVersion()
+        {
+            var appName = PlatformServices.Default.Application.ApplicationName;
+            var json = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, $"{appName}.runtimeconfig.json"));
+            dynamic runtimeconfig = JObject.Parse(json);
+            return runtimeconfig.runtimeOptions.framework.version;
+        }
+    }
+}
